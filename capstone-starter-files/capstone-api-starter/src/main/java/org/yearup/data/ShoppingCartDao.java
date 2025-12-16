163abcafc4ca47b2902cfa1390db1c57e8654731
Zak Khan <sonnymidget@gmail.com>
2025-12-15 22:32:41 -0800

    1) Added things to my caategory class 2) Added the neceary code for for the Catergories class 3)Got my catergories to work with sql 4)In the sqldao I had put the id look up and the get all catergories 5) completed the sql methods and tested the links all works 6) Found the first bug in the MySqlProductDao the search bug was caused by incorrect price filtering logic. I fixed it by properly applying both minimum and maximum price conditions while keeping the original query structure 7) ProductsController put endpoint where create was mistakenly used instead of update, causing duplicate products 8) Made the ShoppingCartController class and the shoping cart dao as well

--- capstone-starter-files/capstone-api-starter/src/main/java/org/yearup/data/ShoppingCartDao.java
+++ capstone-starter-files/capstone-api-starter/src/main/java/org/yearup/data/ShoppingCartDao.java
@@ -2,8 +2,11 @@ package org.yearup.data;
 
 import org.yearup.models.ShoppingCart;
 
-public interface ShoppingCartDao
-{
+public interface ShoppingCartDao {
     ShoppingCart getByUserId(int userId);
     // add additional method signatures here
+    void addProduct (int userId, int productId);
+    void updateQuantity (int userId, int productId, int quantity);
+    void clearCart (int userId);
 }
+
