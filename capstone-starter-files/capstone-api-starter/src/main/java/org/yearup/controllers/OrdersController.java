13f8d215d7b609a781124529b99dbd1366ae065b
Zak Khan <sonnymidget@gmail.com>
2025-12-17 21:58:09 -0800

    1) Added things to my caategory class 2) Added the neceary code for for the Catergories class 3)Got my catergories to work with sql 4)In the sqldao I had put the id look up and the get all catergories 5) completed the sql methods and tested the links all works 6) Found the first bug in the MySqlProductDao the search bug was caused by incorrect price filtering logic. I fixed it by properly applying both minimum and maximum price conditions while keeping the original query structure 7) ProductsController put endpoint where create was mistakenly used instead of update, causing duplicate products 8) Made the ShoppingCartController class and the shoping cart dao as well 9) made mySqlShoppingCartDao class 10) Finished coding the mySqlShoppingCartDao class 11) Making the ProfileController class also had to fix the admin delete function 12) added methods to profiledao and userdoa 13) Started to work on my MySqlProfileDao 14)Finished the MySqlProfileDao class and is working on postman 15) Fixed a problem in the profile controller class 16) Made my OrdersController 17) Made my OrderDao

--- capstone-starter-files/capstone-api-starter/src/main/java/org/yearup/controllers/OrdersController.java
+++ capstone-starter-files/capstone-api-starter/src/main/java/org/yearup/controllers/OrdersController.java
@@ -1,4 +1,39 @@
 package org.yearup.controllers;
 
-public class OrdersController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.yearup.data.ShoppingCartDao;
+import org.yearup.data.UserDao;
+
+import java.security.Principal;
+
+@RestController
+@RequestMapping("/orders")
+@CrossOrigin
+@PreAuthorize("isAuthenticated()")
+public class OrdersController
+{
+    private ShoppingCartDao shoppingCartDao;
+    private OrderDao orderDao;
+    private UserDao userDao;
+
+    @Autowired
+    public OrdersController (ShoppingCartDao shoppingCartDao,
+                             OrderDao orderDao,
+                             UserDao userDao)
+    {
+        this.shoppingCartDao = shoppingCartDao;
+        this.orderDao = orderDao;
+        this.userDao = userDao;
+    }
+
+    @PostMapping
+    public void checkour (Principal principal)
+    {
+
+    }
 }
