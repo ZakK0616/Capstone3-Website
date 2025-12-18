45a2f2a339b1d3b914e776c3405dc78c461c0161
Zak Khan <sonnymidget@gmail.com>
2025-12-17 22:36:44 -0800

    1) Added things to my caategory class 2) Added the neceary code for for the Catergories class 3)Got my catergories to work with sql 4)In the sqldao I had put the id look up and the get all catergories 5) completed the sql methods and tested the links all works 6) Found the first bug in the MySqlProductDao the search bug was caused by incorrect price filtering logic. I fixed it by properly applying both minimum and maximum price conditions while keeping the original query structure 7) ProductsController put endpoint where create was mistakenly used instead of update, causing duplicate products 8) Made the ShoppingCartController class and the shoping cart dao as well 9) made mySqlShoppingCartDao class 10) Finished coding the mySqlShoppingCartDao class 11) Making the ProfileController class also had to fix the admin delete function 12) added methods to profiledao and userdoa 13) Started to work on my MySqlProfileDao 14)Finished the MySqlProfileDao class and is working on postman 15) Fixed a problem in the profile controller class 16) Made my OrdersController 17) Made my OrderDao 18) finished the OrderDao and OrdersController 19) Finished my MySqlOrderDao code

---
+++ capstone-starter-files/capstone-api-starter/src/main/java/org/yearup/data/mysql/MySqlOrderDao.java
@@ -0,0 +1,70 @@
+package org.yearup.data.mysql;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.yearup.data.OrderDao;
+
+import javax.sql.DataSource;
+import java.math.BigDecimal;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+@Component
+public class MySqlOrderDao extends MySqlDaoBase implements OrderDao
+{
+    @Autowired
+    public MySqlOrderDao(DataSource dataSource)
+    {
+        super(dataSource);
+    }
+
+    @Override
+    public int createOrder(int userId, BigDecimal total) {
+       String sql = """
+               INSERT INTO orders (user_id, order_date, total)
+               VALUES (? NOW()< ?)
+               """;
+       try (Connection connection = getConnection();
+            PreparedStatement statement = connection.prepareStatement(sql, PreparedStatement.RETURN_GENERATED_KEYS))
+       {
+           statement.setInt(1 , userId);
+           statement.setBigDecimal(2, total);
+           statement.executeUpdate();
+
+           ResultSet keys = statement.getGeneratedKeys();
+           if (keys.next()) {
+               return keys.getInt(1);
+           }
+
+           return -1;
+       }
+catch (SQLException e)
+{
+    throw new RuntimeException("Error catching your order", e);
+}
+    }
+
+    @Override
+    public void addLineItem(int orderId, int productId, int quantity, BigDecimal price) {
+        String sql = """
+                INSERT INTO order_line_items (order_id, product_id, quantity, price)
+                VALUES (?, ?, ?, ?)
+                """;
+        try (Connection connection = getConnection();
+        PreparedStatement statement = connection.prepareStatement(sql))
+        {
+            statement.setInt(1, orderId);
+            statement.setInt(2, productId);
+            statement.setBigDecimal(4, price);
+            statement.executeUpdate();
+        }
+        catch (SQLException e)
+        {
+            throw new RuntimeException("Error creating order line item", e);
+
+        }
+
+    }
+}
